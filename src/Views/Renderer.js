dae3b58152e68eb26b158f0cae94fa3e5fbf99ee
VovaSanduliak <vovasanduliak@gmail.com>
2024-08-05 19:14:56 +0300

    create custom renderer class and add resize and fullScreen listeners

--- src/Views/Renderer.js
+++ src/Views/Renderer.js
@@ -1,13 +1,45 @@
 import * as THREE from 'three';
 
-const createRenderer = () => {
-  const renderer = new THREE.WebGLRenderer({});
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.shadowMap.enabled = true;
-  document.body.appendChild(renderer.domElement);
+export default class Renderer {
+  _threeRenderer = null;
+  canvasEl = null;
+  _camera;
 
-  return renderer;
-};
+  constructor() {
+    this.canvasEl = document.getElementById('canvas');
+    this._threeRenderer = new THREE.WebGLRenderer({ canvas: this.canvasEl });
 
-export { createRenderer };
+    this._threeRenderer.setSize(window.innerWidth, window.innerHeight);
+    this._threeRenderer.setPixelRatio(window.devicePixelRatio);
+    this._threeRenderer.shadowMap.enabled = true;
+    document.body.appendChild(this._threeRenderer.domElement);
+
+    this._setEventListeners();
+  }
+
+  render = (scene, camera) => {
+    this._camera = camera;
+    this._threeRenderer.render(scene, camera);
+  };
+
+  _setEventListeners = () => {
+    window.addEventListener('resize', () => {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+
+      this._camera.aspect = width / height;
+      this._camera.updateProjectionMatrix();
+
+      this._threeRenderer.setSize(width, height);
+      this._threeRenderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    });
+
+    window.addEventListener('dblclick', () => {
+      if (document.fullscreenElement) {
+        // document.exitFullscreen();
+      } else {
+        this.canvasEl.requestFullscreen();
+      }
+    });
+  };
+}
